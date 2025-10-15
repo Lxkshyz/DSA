281f8be53ad5db7a223e8a0c6e842d56bdfd7053
Lakshya <lakshyakadian008@gmail.com>
2025-10-15 22:01:13 +0530

    Linear Searching Using recursion

--- Recursion_Problems/LinearSearch.java
+++ Recursion_Problems/LinearSearch.java
@@ -3,17 +3,22 @@ package Recursion_Problems;
 class LinearSearch{
     public static void main(String[] args){
         int[] exampleArr = {1,7,4,3,2,5,6,8};
-        // Approach for linear Search, 
-        System.out.print(Search(exampleArr, 0, 0));
+        
+        if (Search(exampleArr, 0, 0) == -1){
+            System.out.print("Index not Found");
+        }
+        else{
+            System.out.print("Index Found at " + Search(exampleArr, 0, 0));
+        }
     }
 
     public static int Search(int[] arr, int target, int index){
-        if (index == arr.length-1){
+        if (index == arr.length-1){             // Base condition - 1 : if array limit exceeds return -1
             return -1;
         }
-        else if (arr[index] == target){
+        else if (arr[index] == target){         // Base condition - 2 : if target is found at arr[index], return index
             return index;
         }
-        return Search(arr, target, ++index);
+        return Search(arr, target, ++index);    // Recurse condition : Index + 1 post-fix used to avoid overflow.
     }
 }
