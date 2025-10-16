4e411fc6eb642cb979e879e544e72ef3516800de
Lakshya <lakshyakadian008@gmail.com>
2025-10-16 16:34:46 +0530

    Array Index return

--- Recursion_Problems/ArrayListReturn.java
+++ Recursion_Problems/ArrayListReturn.java
@@ -1,19 +1,22 @@
 package Recursion_Problems;
-import java.util.Arrays;
+import java.util.ArrayList;
 
 public class ArrayListReturn {
     public static void main(String[] args) {
-        int[] example_arr1 = {1,2,3,4,4,4};
-        int[] result = new int[3];
-        System.out.println(Arrays.toString(returnArrayList(example_arr1, result, 0, 0, 4)));
-
+        int[] example_arr1 = {1,2,3,4,4,4,5,5,6,6,6,6,6,6};
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        System.out.print("[ ");
+        for (int number : returnArrayList(example_arr1, result, 0, 0, 6)) {
+            System.out.print(number + " ");
+        }
+        System.out.print("]");
     }
-    public static int[] returnArrayList(int[] arr, int[] result , int index, int i , int target){
+    public static ArrayList<Integer> returnArrayList(int[] arr, ArrayList<Integer> result , int index, int i , int target){
         if (index == arr.length){
             return result;
         }
         if(arr[index] == target){ 
-            result[i++] = index;
+            result.add(index);
         }
         return returnArrayList(arr, result, ++index, i, target);
     }
