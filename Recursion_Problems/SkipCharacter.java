a26fc7f8342c31b34631b1e5a8dd92878b9eb0d8
Lakshya <lakshyakadian008@gmail.com>
2025-10-26 12:12:10 +0530

    Skip specific character from string using recursion

--- Recursion_Problems/SkipCharacter.java
+++ Recursion_Problems/SkipCharacter.java
@@ -4,14 +4,15 @@ package Recursion_Problems;
 public class SkipCharacter {
     public static void main(String[] args) {
         String a = "baccad";
-        System.out.println(skip(a, 'a', 0, ""));
+        System.out.println(skip(a, 'c', 0));
     }
 
-    public static String skip(String a, char toSkip, int count, String ans) {
-        if (count == a.length()) return ans;
-        if (a.charAt(count) != toSkip) {
-            ans += a.charAt(count);
+    public static String skip(String a, char toSkip, int count) {
+        if (count == a.length()) return "";
+        char ch = a.charAt(count);
+        if (ch != toSkip) {
+            return ch + skip(a, toSkip, ++count);
         }
-        return skip(a, toSkip, ++count, ans);
+        return skip(a, toSkip, ++count);
     }
 }
